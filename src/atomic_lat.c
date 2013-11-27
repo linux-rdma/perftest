e2b4e380673bb9596edfa6dbe8a792d8486c6330
Gil Rockah <gilr@mellanox.com>
2013-11-27 17:20:30 +0200

    Perftest is now Version 5.1 . before the test, both sides exchanging MTU values, and choosing the minimum.
    Version 5.1 is compatible with version 5.0
    
    Change-Id: I26cd88f3734ac5e15dc5abdce871e1d3e83bb957
    Signed-off-by: Gil Rockah <gilr@mellanox.com>

--- src/atomic_lat.c
+++ src/atomic_lat.c
@@ -98,7 +98,33 @@ int main(int argc, char *argv[]) {
 	}
 
 	// See if MTU and link type are valid and supported.
-	if (check_link_and_mtu(ctx.context,&user_param)) {
+	if (check_link(ctx.context,&user_param)) {
+		fprintf(stderr, " Couldn't get context for the device\n");
+		return FAILURE;
+	}
+
+	// copy the relevant user parameters to the comm struct + creating rdma_cm resources.
+	if (create_comm_struct(&user_comm,&user_param)) {
+		fprintf(stderr," Unable to create RDMA_CM resources\n");
+		return 1;
+	}
+
+	if (user_param.machine == SERVER) {
+		printf("\n************************************\n");
+		printf("* Waiting for client to connect... *\n");
+		printf("************************************\n");
+	}
+
+	// Initialize the connection and print the local data.
+	if (establish_connection(&user_comm)) {
+		fprintf(stderr," Unable to init the socket connection\n");
+		return FAILURE;
+	}
+
+	exchange_versions(&user_comm, &user_param);
+
+	// See if MTU and link type are valid and supported.
+	if (check_mtu(ctx.context,&user_param, &user_comm)) {
 		fprintf(stderr, " Couldn't get context for the device\n");
 		return FAILURE;
 	}
@@ -113,12 +139,6 @@ int main(int argc, char *argv[]) {
 
 	// Allocating arrays needed for the test.
 	alloc_ctx(&ctx,&user_param);
-
-	// copy the rellevant user parameters to the comm struct + creating rdma_cm resources.
-	if (create_comm_struct(&user_comm,&user_param)) {
-		fprintf(stderr," Unable to create RDMA_CM resources\n");
-		return 1;
-	}
 
 	// Create (if nessacery) the rdma_cm ids and channel.
 	if (user_param.work_rdma_cm == ON) {
@@ -160,14 +180,6 @@ int main(int argc, char *argv[]) {
 
 	for (i=0; i < user_param.num_of_qps; i++)
 		ctx_print_pingpong_data(&my_dest[i],&user_comm);
-
-	// Init the connection and print the local data.
-	if (establish_connection(&user_comm)) {
-		fprintf(stderr," Unable to init the socket connection\n");
-		return 1;
-	}
-
-	exchange_versions(&user_comm, &user_param);
 
 	// shaking hands and gather the other side info.
 	if (ctx_hand_shake(&user_comm,my_dest,rem_dest)) {
