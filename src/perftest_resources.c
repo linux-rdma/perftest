f39806214ccd749d49caee401de5f9470189df21
Gil Rockah <gilr@mellanox.com>
2013-12-23 11:35:22 +0200

    fixed a bug that the tests cannot run with size < 8. it was related to a change in increase local address calculation.
    Change-Id: Ic49caf95f4c994ed6154e50db6b39968cd045ada
    Signed-off-by: Gil Rockah <gilr@mellanox.com>

--- src/perftest_resources.c
+++ src/perftest_resources.c
@@ -1639,7 +1639,6 @@ int run_iter_bw(struct pingpong_context *ctx,struct perftest_parameters *user_pa
     struct ibv_send_wr *bad_wr = NULL;
     struct ibv_wc 	   *wc = NULL;
 	int num_of_qps = user_param->num_of_qps;
-	uint64_t inc_size = INC(user_param->size);
 
 	ALLOCATE(wc ,struct ibv_wc ,CTX_POLL_BATCH);
 
@@ -1689,10 +1688,10 @@ int run_iter_bw(struct pingpong_context *ctx,struct perftest_parameters *user_pa
 
 				if (user_param->post_list == 1 && user_param->size <= (cycle_buffer / 2)) {
 
-						increase_loc_addr(ctx->wr[index].sg_list,inc_size,ctx->scnt[index],ctx->my_addr[index],0);
+						increase_loc_addr(ctx->wr[index].sg_list,user_param->size,ctx->scnt[index],ctx->my_addr[index],0);
 
 						if (user_param->verb != SEND)
-							increase_rem_addr(&ctx->wr[index],inc_size,ctx->scnt[index],ctx->rem_addr[index],user_param->verb);
+							increase_rem_addr(&ctx->wr[index],user_param->size,ctx->scnt[index],ctx->rem_addr[index],user_param->verb);
 				}
 
 				ctx->scnt[index] += user_param->post_list;
@@ -1780,7 +1779,6 @@ int run_iter_bw_server(struct pingpong_context *ctx, struct perftest_parameters 
 	struct ibv_recv_wr  *bad_wr_recv = NULL;
 	int firstRx = 1;
 	int size_per_qp = (user_param->use_srq) ? user_param->rx_depth/user_param->num_of_qps : user_param->rx_depth;
-	uint64_t inc_size = INC(user_param->size);
 
 	ALLOCATE(wc ,struct ibv_wc ,CTX_POLL_BATCH);
 
@@ -1844,7 +1842,7 @@ int run_iter_bw_server(struct pingpong_context *ctx, struct perftest_parameters 
 
 						if (SIZE(user_param->connection_type,user_param->size,!(int)user_param->machine) <= (cycle_buffer / 2)) {
 							increase_loc_addr(ctx->rwr[wc[i].wr_id].sg_list,
-											  inc_size,
+											  user_param->size,
 											  rcnt_for_qp[wc[i].wr_id] + size_per_qp,
 											  ctx->rx_buffer_addr[wc[i].wr_id],
 											  user_param->connection_type);
@@ -2009,7 +2007,6 @@ int run_iter_bi(struct pingpong_context *ctx,
 	// This is to ensure SERVER will not start to send packets before CLIENT start the test.
 	int before_first_rx = ON;
 	int size_per_qp = (user_param->use_srq) ? user_param->rx_depth/user_param->num_of_qps : user_param->rx_depth;
-	uint64_t inc_size = INC(user_param->size);
 
 	ALLOCATE(wc_tx,struct ibv_wc,CTX_POLL_BATCH);
 	ALLOCATE(rcnt_for_qp,int,user_param->num_of_qps);
@@ -2063,7 +2060,7 @@ int run_iter_bi(struct pingpong_context *ctx,
 				}
 
 				if (user_param->post_list == 1 && user_param->size <= (cycle_buffer / 2))
-					increase_loc_addr(ctx->wr[index].sg_list,inc_size,ctx->scnt[index],ctx->my_addr[index],0);
+					increase_loc_addr(ctx->wr[index].sg_list,user_param->size,ctx->scnt[index],ctx->my_addr[index],0);
 
 				ctx->scnt[index] += user_param->post_list;
 				totscnt += user_param->post_list;
@@ -2124,7 +2121,7 @@ int run_iter_bi(struct pingpong_context *ctx,
 
 					if (SIZE(user_param->connection_type,user_param->size,!(int)user_param->machine) <= (cycle_buffer / 2)) {
 						increase_loc_addr(ctx->rwr[wc[i].wr_id].sg_list,
-										  inc_size,
+										  user_param->size,
 										  rcnt_for_qp[wc[i].wr_id] + size_per_qp -1,
 										  ctx->rx_buffer_addr[wc[i].wr_id],user_param->connection_type);
 					}
